e73d69fa5faf0cf61674f05f3c925a34cf87709d
jalec <jaleckc@gmail.com>
2022-08-31 09:30:20 -0700

    update

--- src/test/java/dev/canlapan/ScoreApplicationTests.java
+++ src/test/java/dev/canlapan/ScoreApplicationTests.java
@@ -1,9 +1,10 @@
 package dev.canlapan;
 
+import dev.canlapan.score.ScoreApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ScoreApplication.class)
 class ScoreApplicationTests {
 
 	@Test
